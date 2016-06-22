d9ab5cb7e89ea2ff95fe12d72738af0005e63a42
SemanticBeeng <nick@semanticbeeng.com>
2016-06-22 09:47:04 -0700

    fix: to allow paswordless ssh from the devopscontrol-vm

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ export cwd=$(pwd)/config
 docker run -it \
     -d -p 2222:22 \
     -v $cwd/ansible_id_rsa.pub:/home/ubuntu/.ssh/authorized_keys \
+    -v $cwd/ansible_id_rsa.pub:/root/.ssh/authorized_keys \
     synergo/ansible-target:0.1 
 
 
