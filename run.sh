2226ee6225dfba9aff5b42531ab1232e177032a3
SemanticBeeng <nick@semanticbeeng.com>
2016-06-22 10:46:47 -0700

    fix: sethostname at start

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 export cwd=$(pwd)/config
 
 docker run -it \
+    -h smalldatafierce-vm \
     -d -p 2222:22 \
     -v $cwd/ansible_id_rsa.pub:/home/ubuntu/.ssh/authorized_keys \
     -v $cwd/ansible_id_rsa.pub:/root/.ssh/authorized_keys \
